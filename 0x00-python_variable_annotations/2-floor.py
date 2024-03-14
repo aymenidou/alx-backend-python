9778efab77a0c49be7b61feff98bec441bc5aea3
Idouahman Aymen <aymanidou@gmail.com>
2024-03-14 15:13:16 +0000

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,6 +2,6 @@
 '''0x00. Python - Variable Annotations'''
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     '''takes a float n as argument and returns the floor of the float'''
     return int(n)
