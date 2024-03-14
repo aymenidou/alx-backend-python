ab0d933ab57f141c5646103c2992cbc611010fab
Idouahman Aymen <aymanidou@gmail.com>
2024-03-14 11:27:32 +0000

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""0x00. Python - Variable Annotations"""
+
+
+def add(a: float, b: float) -> float:
+    """function add accept two float param and return their sum as float"""
+    return (a+b)
